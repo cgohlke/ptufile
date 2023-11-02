8ebd39c095490d7490866e808aad587089eba90b
Christoph Gohlke <cgohlke@cgohlke.com>
2023-11-01 21:06:53 -0700

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,110 @@
+# ptufile/setup.py
+
+"""Ptufile package Setuptools script."""
+
+import re
+import sys
+
+from setuptools import Extension, setup
+from setuptools.command.build_ext import build_ext as _build_ext
+
+
+def search(pattern, code, flags=0):
+    # return first match for pattern in code
+    match = re.search(pattern, code, flags)
+    if match is None:
+        raise ValueError(f'{pattern!r} not found')
+    return match.groups()[0]
+
+
+with open('ptufile/ptufile.py', encoding='utf-8') as fh:
+    code = fh.read()
+
+version = search(r"__version__ = '(.*?)'", code).replace('.x.x', '.dev0')
+
+description = search(r'"""(.*)\.(?:\r\n|\r|\n)', code)
+
+readme = search(
+    r'(?:\r\n|\r|\n){2}"""(.*)"""(?:\r\n|\r|\n){2}from __future__',
+    code,
+    re.MULTILINE | re.DOTALL,
+)
+readme = '\n'.join(
+    [description, '=' * len(description)] + readme.splitlines()[1:]
+)
+
+if 'sdist' in sys.argv:
+    # update README, LICENSE, and CHANGES files
+
+    with open('README.rst', 'w', encoding='utf-8') as fh:
+        fh.write(readme)
+
+    license = search(
+        r'(# Copyright.*?(?:\r\n|\r|\n))(?:\r\n|\r|\n)+""',
+        code,
+        re.MULTILINE | re.DOTALL,
+    )
+    license = license.replace('# ', '').replace('#', '')
+
+    with open('LICENSE', 'w', encoding='utf-8') as fh:
+        fh.write('BSD 3-Clause License\n\n')
+        fh.write(license)
+
+
+class build_ext(_build_ext):
+    """Delay import numpy until build."""
+
+    def finalize_options(self):
+        _build_ext.finalize_options(self)
+        if isinstance(__builtins__, dict):
+            __builtins__['__NUMPY_SETUP__'] = False
+        else:
+            setattr(__builtins__, '__NUMPY_SETUP__', False)
+        import numpy
+
+        self.include_dirs.append(numpy.get_include())
+
+
+ext_modules = [
+    Extension('ptufile._ptufile', ['ptufile/_ptufile.pyx']),
+]
+
+setup(
+    name='ptufile',
+    version=version,
+    license='BSD',
+    description=description,
+    long_description=readme,
+    long_description_content_type='text/x-rst',
+    author='Christoph Gohlke',
+    author_email='cgohlke@cgohlke.com',
+    url='https://www.cgohlke.com',
+    project_urls={
+        'Bug Tracker': 'https://github.com/cgohlke/ptufile/issues',
+        'Source Code': 'https://github.com/cgohlke/ptufile',
+        # 'Documentation': 'https://',
+    },
+    packages=['ptufile'],
+    package_data={'ptufile': ['py.typed']},
+    python_requires='>=3.9',
+    install_requires=['numpy'],
+    setup_requires=['setuptools', 'numpy'],
+    extras_require={'all': ['xarray', 'tifffile', 'matplotlib']},
+    ext_modules=ext_modules,
+    cmdclass={'build_ext': build_ext},
+    zip_safe=False,
+    platforms=['any'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: BSD License',
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Cython',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+    ],
+)
