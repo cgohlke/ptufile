7a34ec44d716fa4068e003431ae024f7b84e9d48
Christoph Gohlke <cgohlke@cgohlke.com>
2024-09-13 22:15:56 -0700

    Update tests/test_ptufile.py

--- tests/test_ptufile.py
+++ tests/test_ptufile.py
@@ -27,9 +27,12 @@
 # ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
 
+# mypy: allow-untyped-defs
+# mypy: check-untyped-defs=False
+
 """Unittests for the ptufile package.
 
-:Version: 2024.7.13
+:Version: 2024.9.14
 
 """
 
