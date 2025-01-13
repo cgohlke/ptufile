0e9416d493e986a4a1e77561f123fdde2db3f715
Christoph Gohlke <cgohlke@cgohlke.com>
2025-01-13 08:52:03 -0800

    Update tests/test_ptufile.py

--- tests/test_ptufile.py
+++ tests/test_ptufile.py
@@ -6,19 +6,21 @@
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
 #
-# * Redistributions of source code must retain the above copyright
-#   notice, this list of conditions and the following disclaimer.
-# * Redistributions in binary form must reproduce the above copyright
-#   notice, this list of conditions and the following disclaimer in the
-#   documentation and/or other materials provided with the distribution.
-# * Neither the name of the copyright holders nor the names of any
-#   contributors may be used to endorse or promote products derived
-#   from this software without specific prior written permission.
+# 1. Redistributions of source code must retain the above copyright notice,
+#    this list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the documentation
+#    and/or other materials provided with the distribution.
+#
+# 3. Neither the name of the copyright holder nor the names of its
+#    contributors may be used to endorse or promote products derived from
+#    this software without specific prior written permission.
 #
 # THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
 # AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
 # IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-# ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
 # LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
 # CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
 # SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
@@ -32,7 +34,7 @@
 
 """Unittests for the ptufile package.
 
-:Version: 2024.12.28
+:Version: 2025.1.13
 
 """
 
@@ -51,6 +53,7 @@ import pytest
 import xarray
 from numpy.testing import assert_array_equal
 from ptufile import (
+    __version__,
     PhuFile,
     PhuMeasurementMode,
     PhuMeasurementSubMode,
@@ -98,7 +101,7 @@ FILES = [
 @pytest.mark.skipif(__doc__ is None, reason='__doc__ is None')
 def test_version():
     """Assert ptufile versions match docstrings."""
-    ver = ':Version: ' + ptufile.__version__
+    ver = ':Version: ' + __version__
     assert ver in __doc__
     assert ver in ptufile.__doc__
 
@@ -1197,6 +1200,16 @@ def test_issue_pixeltime_zero():
         assert im.shape == (512, 512)
         assert im.dtype == numpy.uint32
         assert im[281, 373] == 25
+
+
+def test_issue_number_records_zero(caplog):
+    """Test PTU with TTResult_NumberOfRecords=0."""
+    # https://github.com/cgohlke/ptufile/issues/2
+    fname = HERE / 'FLIM_number_records_zero.ptu'
+    with PtuFile(fname) as ptu:
+        with caplog.at_level(logging.WARNING):
+            assert ptu.number_records == 12769472
+        assert 'TTResult_NumberOfRecords is zero' in caplog.text
 
 
 def test_issue_record_number(caplog):
