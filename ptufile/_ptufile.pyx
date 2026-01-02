c45b6bc029c8e3010e653ee9204e6859eb7df3e7
Christoph Gohlke <cgohlke@cgohlke.com>
2026-01-01 17:43:12 -0800

    Update ptufile/_ptufile.pyx

--- ptufile/_ptufile.pyx
+++ ptufile/_ptufile.pyx
@@ -1,13 +1,12 @@
 # _ptufile.pyx
 # distutils: language = c
-# cython: language_level = 3
 # cython: boundscheck = False
 # cython: wraparound = False
 # cython: cdivision = True
 # cython: nonecheck = False
 # cython: freethreading_compatible = True
 
-# Copyright (c) 2023-2025, Christoph Gohlke
+# Copyright (c) 2023-2026, Christoph Gohlke
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
